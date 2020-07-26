ff765c0f0dbccf96d5531813d5e56872a25aee2d
Priyanka Hotchandani <p.hotchandani@somaiya.edu>
2020-07-26 19:59:15 +0530

    [Task 1]

---
+++ Task 1/1.2 Flip horizontally.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+count = 0
+def count_frames(count):
+        if count % 2 !=0:
+            cv2.imshow('flipped',flipVert)
+        else:
+            cv2.imshow('flipped',frame)
+
+
+while True:
+    ret, frame = cap.read()
+    flipVert = cv2.flip(frame, 1)
+    count=count+1
+
+    count_frames(count)
+   
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+
+#out.release()
+cv2.destroyAllWindows()    
