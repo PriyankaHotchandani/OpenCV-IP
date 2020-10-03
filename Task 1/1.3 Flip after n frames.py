7dbb98d9246a188c2170b47b9b9fc8925dcdcd8e
PriyankaHotchandani <68430517+PriyankaHotchandani@users.noreply.github.com>
2020-10-03 10:52:15 +0530

    Update 1.3 Flip after n frames.py

--- Task 1/1.3 Flip after n frames.py
+++ Task 1/1.3 Flip after n frames.py
@@ -5,14 +5,14 @@ cap = cv2.VideoCapture(0)
 count = 0
 def count_frames(count):
         if count % 5 ==0:
-            cv2.imshow('flipped',flipVert)
+            cv2.imshow('flipped',flip)
         else:
             cv2.imshow('flipped',frame)
 
 
 while True:
     ret, frame = cap.read()
-    flipVert = cv2.flip(frame, 0)
+    flip = cv2.flip(frame, 0)
     count=count+1
 
     count_frames(count)
